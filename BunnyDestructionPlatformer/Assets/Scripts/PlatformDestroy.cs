0984e3e006b4ce2fa63cfae5ec083c0a365116a4
Neil <nalexa200@caledonian.ac.uk>
2018-04-07 01:08:38 +0100

    Platform deletion added to manage memory

---
+++ BunnyDestructionPlatformer/Assets/Scripts/PlatformDestroy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformDestroy : MonoBehaviour {
+
+    public GameObject platformDestroyPt;
+
+	// Use this for initialization
+	void Start () {
+
+        platformDestroyPt = GameObject.Find("PlatformDestroyPt"); /*Finds the current point used to determine where to destroy platforms*/
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if (transform.position.x < platformDestroyPt.transform.position.x ) /*If position is behind platform destroy point...*/
+        {
+            Destroy(gameObject); /*Destroy game object*/
+        }
+
+	}
+}
