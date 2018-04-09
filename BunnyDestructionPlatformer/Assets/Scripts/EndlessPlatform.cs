0fa0e2ec9780079ba53fef5b3d2c08e0eaee8c3d
Neil <nalexa200@caledonian.ac.uk>
2018-04-09 02:42:36 +0100

    Added pause functionality and random platform gaps

--- BunnyDestructionPlatformer/Assets/Scripts/EndlessPlatform.cs
+++ BunnyDestructionPlatformer/Assets/Scripts/EndlessPlatform.cs
@@ -10,6 +10,9 @@ public class EndlessPlatform : MonoBehaviour {
 
     private float platformWidth;
 
+    public float platformGapMin;
+    public float platformGapMax;
+
 
 	// Use this for initialization
 	void Start () {
@@ -23,6 +26,8 @@ public class EndlessPlatform : MonoBehaviour {
 		
         if (transform.position.x < genPoint.position.x)
         {
+            distanceBetween = Random.Range(platformGapMin, platformGapMax); /*Generate distance between platforms randomly between max/min allowed distance*/
+
             transform.position = new Vector3(transform.position.x + platformWidth + distanceBetween, transform.position.y, transform.position.z);
 
             Instantiate(objPlatform, transform.position, transform.rotation); /*Create new platform*/
--- BunnyDestructionPlatformer/Assets/Scripts/SceneManager.cs
+++ BunnyDestructionPlatformer/Assets/Scripts/SceneManager.cs
@@ -1,12 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneManager : MonoBehaviour {
+
 
     public void LoadScene(int level)
     {
         Application.LoadLevel(level);
+        Time.timeScale = 1f;
     }
+
+
+    /*Quit game script*/
+    public void QuitGame()
+    {
+        Debug.Log("Y U QUIT MY GAME :(");
+        Application.Quit(); /*Quit game when called*/
+    }
+
 
 }
