47ee03fa1c13efa59327fc2a3266d8e704ff46fa
Neil <nalexa200@caledonian.ac.uk>
2018-04-10 16:55:28 +0100

    High score saving added, project test build created

--- BunnyDestructionPlatformer/Assets/Scripts/ScoreManager.cs
+++ BunnyDestructionPlatformer/Assets/Scripts/ScoreManager.cs
@@ -19,7 +19,12 @@ public class ScoreManager : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+
+		if (PlayerPrefs.HasKey ("HighScore"))   // If HighScore has been stored via player prefs on previous playthrough
+        {
+            highScoreCount = PlayerPrefs.GetFloat("HighScore"); // Set high score to any previous highest score
+        }
+
 	}
 	
 	// Update is called once per frame
@@ -33,6 +38,7 @@ public class ScoreManager : MonoBehaviour {
         if (scoreCount > highScoreCount)
         {
             highScoreCount = scoreCount;    // Sets the new high score = current score count if high score is beaten
+            PlayerPrefs.SetFloat("HighScore", highScoreCount);  // Saves value names HighScore - which is highScoreCount
         }
 
         scoreText.text = "Score: " + Mathf.Round (scoreCount);    // Print Score: with current player
